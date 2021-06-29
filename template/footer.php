7a058fb18b9a960385a161c0ec466605cf3c12a4
insanz01 <insankamil002@gmail.com>
2021-06-29 10:46:17 +0700

    Add templating menu

---
+++ template/footer.php
@@ -0,0 +1,4 @@
+  <script src="http://localhost/googlemaps/assets/js/bootstrap.min.js"></script>
+  <script src="http://localhost/googlemaps/assets/custom/script.js"></script>
+</body>
+</html>
---
+++ template/navbar.php
@@ -0,0 +1,18 @@
+<nav>
+  <div class="logo">
+    <h4>Isi kena</h4>
+  </div>
+
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="index.php">Maps</a></li>
+    <li><a href="#">About Us</a></li>
+  </ul>
+
+  <div class="menu-toggle">
+    <input type="checkbox">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+</nav>
